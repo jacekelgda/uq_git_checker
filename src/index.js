84c18e09942338080c8d4977f283e0ab4ff0fb5a
jacekelgda <jacekelgda@gmail.com>
2017-11-21 17:08:52 +0100

    Fetching branches --wip

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-# TBD
+import rp from 'request-promise'
+
+const fetchBranches = () => {
+  const options = {
+      uri: 'https://api.github.com/repos/jacekelgda/uq_git_w1_student/branches',
+      headers: {
+          'User-Agent': 'Request-Promise'
+      },
+      json: true
+  }
+  rp(options)
+    .then((response) => {
+        for ( let i=0; i<response.length; i+=1 ) {
+          console.log(response[i].name)
+        }
+    })
+    .catch((err) => {
+        console.log(err)
+    });
+}
+
+fetchBranches()
